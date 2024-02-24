337ee0b2510d29b8b512957c2c26bb0cedfa19a5
YuYoHan <zxzz8014@naver.com>
2024-02-24 21:09:03 +0900

    [Study] subquery

--- ex01.sql
+++ ex01.sql
@@ -28,7 +28,42 @@ where id = DSTINCT_E.empl_id;
 
 # ID가 7 혹은 12인 임직원이 참여한 프로젝트의 ID와 이름을 알고 싶다.
 # exists : 존재하는 것을 찾아준다.
+select P.id, P.name
+	from project P
+    where exists (
+		select W.proj_id
+        from works W
+        where W.empl_id in(7, 12)
+        );
+        
+# 위에꺼와 같은 의미
+select P.id, P.name
+	from project P
+    where id in(
+		select W.proj_id
+        from works W
+        where W.empl_id in(7, 12)
+        );        
 
-
+# 2000년대생이 없는 부서의 ID와 이름을 알고싶다.
+select D.id, D.name
+from department as D
+where not exists(
+	select * from employee E
+    where E.dept_id = D.id and E.birth_date >= '2000-01-01'
+    );
+    
+# 리더보다 높은 연봉을 받는 부서원을 가진 리더의 Id와 이름과 연봉을 알고 싶다.
+# <>는 != 와 같다.
+# any : 단 하나라도 조건에 맞는게 있다면 true
+select E.id, E.name, E.salary
+from department D, employee E
+where D.leader_id = E.id and E.salary < any(
+		select salary
+        from employee
+        where id <> D.leader_id and dept_id = E.dept_id
+        );
+        
+        
     
     
